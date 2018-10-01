0e5d2da73de3b21e637ac47073b44c275513165d
novakvova <novakvova@gmail.com>
2018-10-01 18:44:36 +0300

    Install jsTree

--- WebShop/Models/Entities/Category.cs
+++ WebShop/Models/Entities/Category.cs
@@ -15,8 +15,10 @@ namespace WebShop.Models.Entities
         [Display(Name = "Category Name")]
         [Required(ErrorMessage = "The category name cannot be blank")]
         [StringLength(50, MinimumLength = 3, ErrorMessage = "Please enter a category name between 3 and 50 characters in length") ] 
-        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$", ErrorMessage = "Please enter a category name beginning with a capital letter and made up of letters and spaces only") ]
         public string Name { get; set; }
+        [ForeignKey("CategoryOf")]
+        public int ? ParentId { get; set; }
+        public Category CategoryOf { get; set; }
         public virtual ICollection<Product> Products { get; set; }
     }
 }
